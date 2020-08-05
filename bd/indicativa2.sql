941cbb56aae81995a27ba93778e7f7b1fbffb5f1
afmoreno355 <afmoreno355@misena.edu.co>
2020-08-05 17:58:12 -0500

    REPO indicativa con bd 3

--- bd/indicativa2.sql
+++ bd/indicativa2.sql
@@ -16,7 +16,7 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
-SET default_tablespace = '';
+SET default_tablespace = ''; 
 
 SET default_with_oids = false;
 
